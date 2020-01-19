fed7e31052bb1caf0ad28f7028a6358c9d31f2d2
Mansoor <hussainmansoor876@gmail.com>
2020-01-19 15:49:58 +0500

    Login done

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { loginUser } from '../../Redux/actions/authActions'
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
 import Loader from '../../Components/Loader';
 import 'antd/dist/antd.css';
 import { Form, Icon, Input, Button, Checkbox, notification } from 'antd';
@@ -46,32 +47,6 @@ class Login extends React.Component {
                                             </a>
                                             <p className="text-muted mb-4 mt-3">Enter your email address and password to access admin panel.</p>
                                         </div>
-
-                                        <form action="#">
-
-                                            <div className="form-group mb-3">
-                                                <label for="emailaddress">Email address</label>
-                                                <input className="form-control" type="email" id="emailaddress" required placeholder="Enter your email" />
-                                            </div>
-
-                                            <div className="form-group mb-3">
-                                                <label for="password">Password</label>
-                                                <input className="form-control" type="password" required id="password" placeholder="Enter your password" />
-                                            </div>
-
-                                            <div className="form-group mb-3">
-                                                <div className="custom-control custom-checkbox">
-                                                    <input type="checkbox" className="custom-control-input" id="checkbox-signin" checked />
-                                                    <label className="custom-control-label" for="checkbox-signin">Remember me</label>
-                                                </div>
-                                            </div>
-
-                                            <div className="form-group mb-0 text-center">
-                                                <button className="btn btn-primary btn-block" type="submit"> Log In </button>
-                                            </div>
-
-                                        </form>
-
                                         <Form onSubmit={this.handleSubmit} className="login-form">
                                             <Form.Item>
                                                 {getFieldDecorator('username', {
@@ -99,40 +74,15 @@ class Login extends React.Component {
                                                     valuePropName: 'checked',
                                                     initialValue: true,
                                                 })(<Checkbox>Remember me</Checkbox>)}
-                                                <a className="login-form-forgot btn-primary" href="">
+                                                <Link className="login-form-forgot" to="" style={{ color: '#6658dd' }}>
                                                     Forgot password
-                                                    </a>
+                                                    </Link>
                                                 <Button type="primary" htmlType="submit" className="login-form-button btn-primary btn-block">
                                                     Log in
                                                 </Button>
-                                                Or <a href="">register now!</a>
+                                                Don't have an account? <Link to="" className="login-form-forgot">Sign Up</Link>
                                             </Form.Item>
                                         </Form>
-                                        <div className="text-center">
-                                            <h5 className="mt-3 text-muted">Sign in with</h5>
-                                            <ul className="social-list list-inline mt-3 mb-0">
-                                                <li className="list-inline-item">
-                                                    <a href="javascript: void(0);" className="social-list-item border-primary text-primary"><i className="mdi mdi-facebook"></i></a>
-                                                </li>
-                                                <li className="list-inline-item">
-                                                    <a href="javascript: void(0);" className="social-list-item border-danger text-danger"><i className="mdi mdi-google"></i></a>
-                                                </li>
-                                                <li className="list-inline-item">
-                                                    <a href="javascript: void(0);" className="social-list-item border-info text-info"><i className="mdi mdi-twitter"></i></a>
-                                                </li>
-                                                <li className="list-inline-item">
-                                                    <a href="javascript: void(0);" className="social-list-item border-secondary text-secondary"><i className="mdi mdi-github-circle"></i></a>
-                                                </li>
-                                            </ul>
-                                        </div>
-
-                                    </div>
-                                </div>
-
-                                <div className="row mt-3">
-                                    <div className="col-12 text-center">
-                                        <p> <a href="pages-recoverpw.html" className="text-white-50 ml-1">Forgot your password?</a></p>
-                                        <p className="text-white-50">Don't have an account? <a href="pages-register.html" className="text-white ml-1"><b>Sign Up</b></a></p>
                                     </div>
                                 </div>
 
