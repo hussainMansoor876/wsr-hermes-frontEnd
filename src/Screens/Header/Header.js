8ac7e7e28b9745fca2b5e6eaae09f5badb593f9d
Mansoor <hussainmansoor876@gmail.com>
2020-01-20 22:22:46 +0500

    Header

--- src/Screens/Header/Header.js
+++ src/Screens/Header/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { loginUser } from '../../Redux/actions/authActions'
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { Icon } from 'antd'
 import Loader from '../../Components/Loader';
 import './Header.css'
 import logo from '../../assets/images/logo-dark.png';
@@ -23,15 +24,26 @@ class Header extends React.Component {
                 display: 'flex',
                 paddingTop: 10,
                 marginLeft: 10,
-                marginRight: 10
+                marginRight: 10,
+                justifyContent: 'space-between'
             }}>
-                <Link to="/">
-                    <span><img src={logo} alt="" style={{ height: '45px', marginRight: 10 }} width="80" /></span>
-                </Link>
+                <div style={{ display: 'flex' }}>
+                    <Link to="/">
+                        <span><img src={logo} alt="" style={{ height: '45px', marginRight: 10 }} width="80" /></span>
+                    </Link>
 
-                <h3 className="header-text">
-                    Hermes
+                    <h3 className="header-text">
+                        Hermes
                 </h3>
+                    <div class="container1" onclick="myFunction(this)">
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>
+                    </div>
+                </div>
+                <div>
+                    <img src={'https://avatars0.githubusercontent.com/u/35415573?s=400&u=7585be6ddd43c201b02168c1fe36fd5e33a06bca&v=4'} alt="" style={{ marginRight: 10, borderRadius: 50 }} height="30" width="30" />
+                </div>
             </div>
         )
     }
