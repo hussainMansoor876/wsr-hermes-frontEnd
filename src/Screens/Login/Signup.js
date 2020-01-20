c16385ba0d1161f3fb3094615be5ac7cb94a5bf4
Mansoor <hussainmansoor876@gmail.com>
2020-01-20 16:58:19 +0500

    ADded password

--- src/Screens/Login/Signup.js
+++ src/Screens/Login/Signup.js
@@ -16,6 +16,32 @@ class Signup extends React.Component {
         }
     }
 
+    validateToNextPassword = (rule, value, callback) => {
+        const { form } = this.props;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    };
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const { form } = this.props;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
 
     render() {
         const { getFieldDecorator } = this.props.form;
@@ -46,22 +72,42 @@ class Signup extends React.Component {
                                                     />,
                                                 )}
                                             </Form.Item>
-                                            <Form.Item>
+                                            <Form.Item hasFeedback>
                                                 {getFieldDecorator('password', {
-                                                    rules: [{ required: true, message: 'Please input your Password!' }],
-                                                })(
-                                                    <Input
-                                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                                        type="password"
-                                                        placeholder="Password"
-                                                    />,
-                                                )}
+                                                    rules: [
+                                                        {
+                                                            required: true,
+                                                            message: 'Please input your password!',
+                                                        },
+                                                        {
+                                                            validator: this.validateToNextPassword,
+                                                        },
+                                                    ],
+                                                })(<Input.Password
+                                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                                    placeholder="Enter your Password" />)}
+                                            </Form.Item>
+                                            <Form.Item hasFeedback>
+                                                {getFieldDecorator('confirm', {
+                                                    rules: [
+                                                        {
+                                                            required: true,
+                                                            message: 'Please confirm your password!',
+                                                        },
+                                                        {
+                                                            validator: this.compareToFirstPassword,
+                                                        },
+                                                    ],
+                                                })(<Input.Password
+                                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                                    placeholder="Confirm Password"
+                                                    onBlur={this.handleConfirmBlur} />)}
                                             </Form.Item>
                                             <Form.Item>
                                                 <Button type="primary" htmlType="submit" className="login-form-button btn-block signup-btn">
                                                     Register
                                                 </Button>
-                                                Already have account? <Link to="" className="login-form-forgot" style={{ color: '#1abc9c'}}>Sign In</Link>
+                                                Already have account? <Link to="" className="login-form-forgot" style={{ color: '#1abc9c' }}>Sign In</Link>
                                             </Form.Item>
                                         </Form>
 
