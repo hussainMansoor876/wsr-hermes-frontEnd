4362cd23da00e17b4f58b72935dec22b7ae2487e
Mansoor <hussainmansoor876@gmail.com>
2020-01-21 19:04:56 +0500

    Updated pages

---
+++ src/Screens/ReviewPage/ReviewPage.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { loginUser } from '../../Redux/actions/authActions'
+import { connect } from 'react-redux';
+import Loader from '../../Components/Loader';
+import logo from '../../assets/images/logo-dark.png';
+
+class Dashboard extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+
+
+const mapStateToProps = (state) => {
+    console.log("mapToState", state.authReducer)
+    return {
+        user: state.authReducer.user,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loginUser: (user) => dispatch(loginUser(user)),
+    }
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
