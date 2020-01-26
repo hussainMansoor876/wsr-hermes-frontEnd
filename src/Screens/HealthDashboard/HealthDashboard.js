cc02fd052626e88c80b9bffc8b91ac18a6572315
Mansoor <hussainmansoor876@gmail.com>
2020-01-27 00:29:41 +0500

    working on dashboard

--- src/Screens/HealthDashboard/HealthDashboard.js
+++ src/Screens/HealthDashboard/HealthDashboard.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 import { loginUser } from '../../Redux/actions/authActions'
 import { connect } from 'react-redux';
 import Loader from '../../Components/Loader';
-import logo from '../../assets/images/logo-dark.png';
+import { Doughnut } from 'react-chartjs-2';
 import Header from '../Header/Header'
+
+const data = {
+	labels: [
+		'Red',
+		'Green',
+		'Yellow'
+	],
+	datasets: [{
+		data: [300, 50, 100],
+		backgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		],
+		hoverBackgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		]
+	}]
+};
 
 class Dashboard extends React.Component {
 
@@ -19,7 +40,13 @@ class Dashboard extends React.Component {
         return (
             <div>
                 <Header {...this.props} />
-                Hello
+                <div style={{ backgroundColor: '#E5E5E5' }}>
+                <Doughnut
+                data={data}
+                // width={50}
+                // height={10}
+                 />
+                </div>
             </div>
         )
     }
