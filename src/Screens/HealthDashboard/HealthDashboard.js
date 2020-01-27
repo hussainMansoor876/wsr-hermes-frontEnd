39fccfa6c50638c8d299098c581de98ffec971cb
Mansoor <hussainmansoor876@gmail.com>
2020-01-27 23:49:46 +0500

    done34

--- src/Screens/HealthDashboard/HealthDashboard.js
+++ src/Screens/HealthDashboard/HealthDashboard.js
@@ -39,6 +39,42 @@ class Dashboard extends React.Component {
         }
     }
 
+    range(start, end) {
+        const result = [];
+        for (let i = start; i < end; i++) {
+            result.push(i);
+        }
+        return result;
+    }
+
+    disabledDate(current) {
+        // Can not select days before today and today
+        return current && current < moment().endOf('day');
+    }
+
+    disabledDateTime() {
+        return {
+            disabledHours: () => this.range(0, 24).splice(4, 20),
+            disabledMinutes: () => this.range(30, 60),
+            disabledSeconds: () => [55, 56],
+        };
+    }
+
+    disabledRangeTime(_, type) {
+        if (type === 'start') {
+            return {
+                disabledHours: () => this.range(0, 60).splice(4, 20),
+                disabledMinutes: () => this.range(30, 60),
+                disabledSeconds: () => [55, 56],
+            };
+        }
+        return {
+            disabledHours: () => this.range(0, 60).splice(20, 4),
+            disabledMinutes: () => this.range(0, 31),
+            disabledSeconds: () => [55, 56],
+        };
+    }
+
 
     render() {
         return (
@@ -50,8 +86,8 @@ class Dashboard extends React.Component {
                     </div>
                     <div style={{ display: 'flex', margin: 20, flex: 1, flexDirection: 'row', justifyContent: 'flex-end' }}>
                         <RangePicker
-                            disabledDate={disabledDate}
-                            disabledTime={disabledRangeTime}
+                            disabledDate={this.disabledDate.bind(this)}
+                            disabledTime={this.disabledRangeTime.bind(this)}
                             showTime={{
                                 hideDisabledOptions: true,
                                 defaultValue: [moment('00:00:00', 'HH:mm:ss'), moment('11:59:59', 'HH:mm:ss')],
