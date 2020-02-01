6027986e212159a026a050d00b205c29b80b227c
Mansoor <hussainmansoor876@gmail.com>
2020-02-01 16:41:09 +0500

    finalized

--- src/Screens/Header/Header.js
+++ src/Screens/Header/Header.js
@@ -26,9 +26,9 @@ class Header extends React.Component {
             this.props.history.replace('/')
         }
 
-        if(user && user.role == 'agent' && location.pathname !== '/submission'){
-            this.props.history.replace('/submission')
-        }
+        // if(user && user.role == 'agent' && location.pathname !== '/submission'){
+        //     this.props.history.replace('/submission')
+        // }
     }
 
     logout() {
