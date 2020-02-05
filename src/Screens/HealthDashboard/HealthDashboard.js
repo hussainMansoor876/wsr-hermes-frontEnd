1e80f8de90c6cd6a2c9ad94b452a35071f9700c1
Mansoor <hussainmansoor876@gmail.com>
2020-02-06 00:06:47 +0500

    done

--- src/Screens/HealthDashboard/HealthDashboard.js
+++ src/Screens/HealthDashboard/HealthDashboard.js
@@ -5,7 +5,6 @@ import Loader from '../../Components/Loader';
 import { DatePicker } from 'antd';
 import Header from '../Header/Header'
 import moment from 'moment';
-import PieChart from 'react-minimal-pie-chart';
 import Chart from 'react-apexcharts'
 
 const { RangePicker } = DatePicker;
@@ -45,95 +44,108 @@ class Dashboard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            options: {
+                chart: {
+                    id: 'apexchart-example'
+                },
+                xaxis: {
+                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+                }
+            },
+            series: [{
+                name: 'series-1',
+                data: [30, 40, 45, 50, 49, 60, 70, 91]
+            }]
         }
     }
+}
 
-    range(start, end) {
-        const result = [];
-        for (let i = start; i < end; i++) {
-            result.push(i);
-        }
-        return result;
+range(start, end) {
+    const result = [];
+    for (let i = start; i < end; i++) {
+        result.push(i);
     }
+    return result;
+}
 
-    disabledDate(current) {
-        // Can not select days before today and today
-        return current && current < moment('31/12/2019').endOf('day');
-    }
+disabledDate(current) {
+    // Can not select days before today and today
+    return current && current < moment('31/12/2019').endOf('day');
+}
 
 
-    render() {
-        return (
-            <div>
-                <Header {...this.props} />
-                <div style={{ backgroundColor: '#E5E5E5' }}>
-                    <div className="dashboardHeader" style={{ justifyContent: 'center', alignItems: 'center', paddingTop: 20 }}>
-                        Woodward Square Reality Health Report
+render() {
+    return (
+        <div>
+            <Header {...this.props} />
+            <div style={{ backgroundColor: '#E5E5E5' }}>
+                <div className="dashboardHeader" style={{ justifyContent: 'center', alignItems: 'center', paddingTop: 20 }}>
+                    Woodward Square Reality Health Report
                     </div>
-                    <div style={{ display: 'flex', margin: 20, flex: 1, flexDirection: 'row', justifyContent: 'flex-end' }} className="dateRange">
-                        <RangePicker
-                            disabledDate={this.disabledDate.bind(this)}
-                            format="YYYY-MM-DD"
-                        />
+                <div style={{ display: 'flex', margin: 20, flex: 1, flexDirection: 'row', justifyContent: 'flex-end' }} className="dateRange">
+                    <RangePicker
+                        disabledDate={this.disabledDate.bind(this)}
+                        format="YYYY-MM-DD"
+                    />
+                </div>
+                <div style={{ textAlign: 'center', display: 'block', margin: 10 }}>
+                    <div class="boxes1">
+                        <p className="headingText">Net Revenue</p>
+                        <p className="text">$15.23</p>
+                        <div style={{ display: 'flex' }}>
+                            <p className="textBottom">15.10%</p>
+                            <p className="textBottom1">&nbsp;(31 days)</p>
+                        </div>
                     </div>
-                    <div className="chart1">
-                        <Chart options={options} series={series} type="pie" width={500} height={320} />
+                    <div class="boxes1">
+                        <p className="headingText">Net Revenue</p>
+                        <p className="text">$15.23</p>
+                        <div style={{ display: 'flex' }}>
+                            <p className="textBottom">15.10%</p>
+                            <p className="textBottom1">&nbsp;(31 days)</p>
+                        </div>
                     </div>
-                    <div style={{ textAlign: 'center', display: 'block', margin: 10 }}>
-                        <div class="boxes1">
-                            <p className="headingText">Net Revenue</p>
-                            <p className="text">$15.23</p>
-                            <div style={{ display: 'flex' }}>
-                                <p className="textBottom">15.10%</p>
-                                <p className="textBottom1">&nbsp;(31 days)</p>
-                            </div>
+                    <div class="boxes1">
+                        <p className="headingText">Net Revenue</p>
+                        <p className="text">$15.23</p>
+                        <div style={{ display: 'flex' }}>
+                            <p className="textBottom">15.10%</p>
+                            <p className="textBottom1">&nbsp;(31 days)</p>
                         </div>
-                        <div class="boxes1">
-                            <p className="headingText">Net Revenue</p>
-                            <p className="text">$15.23</p>
-                            <div style={{ display: 'flex' }}>
-                                <p className="textBottom">15.10%</p>
-                                <p className="textBottom1">&nbsp;(31 days)</p>
-                            </div>
+                    </div>
+                    <div class="boxes1">
+                        <p className="headingText">Net Revenue</p>
+                        <p className="text">$15.23</p>
+                        <div style={{ display: 'flex' }}>
+                            <p className="textBottom">15.10%</p>
+                            <p className="textBottom1">&nbsp;(31 days)</p>
                         </div>
-                        <div class="boxes1">
-                            <p className="headingText">Net Revenue</p>
-                            <p className="text">$15.23</p>
-                            <div style={{ display: 'flex' }}>
-                                <p className="textBottom">15.10%</p>
-                                <p className="textBottom1">&nbsp;(31 days)</p>
-                            </div>
+                    </div>
+                    <div class="boxes1">
+                        <p className="headingText">Net Revenue</p>
+                        <p className="text">$15.23</p>
+                        <div style={{ display: 'flex' }}>
+                            <p className="textBottom">15.10%</p>
+                            <p className="textBottom1">&nbsp;(31 days)</p>
                         </div>
-                        <div class="boxes1">
-                            <p className="headingText">Net Revenue</p>
-                            <p className="text">$15.23</p>
-                            <div style={{ display: 'flex' }}>
-                                <p className="textBottom">15.10%</p>
-                                <p className="textBottom1">&nbsp;(31 days)</p>
-                            </div>
-                        </div>
-                        <div class="boxes1">
-                            <p className="headingText">Net Revenue</p>
-                            <p className="text">$15.23</p>
-                            <div style={{ display: 'flex' }}>
-                                <p className="textBottom">15.10%</p>
-                                <p className="textBottom1">&nbsp;(31 days)</p>
-                            </div>
-                        </div>
-                        <div class="boxes1">
-                            <p className="headingText">Net Revenue</p>
-                            <p className="text">$15.23</p>
-                            <div style={{ display: 'flex' }}>
-                                <p className="textBottom">15.10%</p>
-                                <p className="textBottom1">&nbsp;(31 days)</p>
-                            </div>
+                    </div>
+                    <div class="boxes1">
+                        <p className="headingText">Net Revenue</p>
+                        <p className="text">$15.23</p>
+                        <div style={{ display: 'flex' }}>
+                            <p className="textBottom">15.10%</p>
+                            <p className="textBottom1">&nbsp;(31 days)</p>
                         </div>
                     </div>
                 </div>
+                <div className="chart1">
+                    <Chart options={options} series={series} type="pie" width={500} height={320} />
+                    <Chart options={this.state.options} series={this.state.series} type="bar" width={500} height={320} />
+                </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
 }
 
 
