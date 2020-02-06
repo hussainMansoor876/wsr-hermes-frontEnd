97e42d380cf4623ff66c1a13fe3d2a8bdd69ed28
Mansoor <hussainmansoor876@gmail.com>
2020-02-06 13:35:48 +0500

    done

--- src/Screens/HealthDashboard/HealthDashboard.js
+++ src/Screens/HealthDashboard/HealthDashboard.js
@@ -55,7 +55,47 @@ class Dashboard extends React.Component {
             series: [{
                 name: 'series-1',
                 data: [30, 40, 45, 50, 49, 60, 70, 91]
-            }]
+            }],
+            series1: [{
+                name: 'Website Blog',
+                type: 'column',
+                data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160]
+            }, {
+                name: 'Social Media',
+                type: 'line',
+                data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
+            }],
+            options1: {
+                chart: {
+                    height: 350,
+                    type: 'line',
+                },
+                stroke: {
+                    width: [0, 4]
+                },
+                title: {
+                    text: 'Traffic Sources'
+                },
+                dataLabels: {
+                    enabled: true,
+                    enabledOnSeries: [1]
+                },
+                labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
+                xaxis: {
+                    type: 'datetime'
+                },
+                yaxis: [{
+                    title: {
+                        text: 'Website Blog',
+                    },
+
+                }, {
+                    opposite: true,
+                    title: {
+                        text: 'Social Media'
+                    }
+                }]
+            },
         }
     }
 
@@ -144,6 +184,9 @@ class Dashboard extends React.Component {
                         </div>
                         <div className="chart1">
                             <Chart options={this.state.options} series={this.state.series} type="bar" width={500} height={320} />
+                        </div>
+                        <div className="chart1">
+                            <Chart options={this.state.options1} series={this.state.series1} type="line" width={500} height={320} />
                         </div>
                     </div>
                 </div>
