4d4103eb080618c24e20d9e0ae398ad21d024b79
Mansoor <hussainmansoor876@gmail.com>
2020-02-06 22:32:56 +0500

    done

--- src/Screens/HealthDashboard/HealthDashboard.js
+++ src/Screens/HealthDashboard/HealthDashboard.js
@@ -229,29 +229,33 @@ class Dashboard extends React.Component {
                                     </Select>
                                 </div>
                                 <div className="div2">
-                                    <div className="div3">
-                                        <h1 className="divBody">Deals</h1>
-                                        <h1 className="divBody">4</h1>
+                                    <div>
+                                        <div className="div3">
+                                            <h1 className="divBody">Deals</h1>
+                                            <h1 className="divBody">4</h1>
+                                        </div>
+                                        <div className="div3">
+                                            <h1 className="divBody">Sales Volume</h1>
+                                            <h1 className="divBody">$3,113,800</h1>
+                                        </div>
+                                        <div className="div3">
+                                            <h1 className="divBody">WSR Revenue</h1>
+                                            <h1 className="divBody">$4,500</h1>
+                                        </div>
                                     </div>
-                                    <div className="div3">
-                                        <h1 className="divBody">Sales Volume</h1>
-                                        <h1 className="divBody">$3,113,800</h1>
-                                    </div>
-                                    <div className="div3">
-                                        <h1 className="divBody">WSR Revenue</h1>
-                                        <h1 className="divBody">$4,500</h1>
-                                    </div>
-                                    <div className="div3">
-                                        <h1 className="divBody">Commission Earned</h1>
-                                        <h1 className="divBody">$76,259</h1>
-                                    </div>
-                                    <div className="div3">
-                                        <h1 className="divBody">Cap Fill</h1>
-                                        <h1 className="divBody">100%</h1>
-                                    </div>
-                                    <div className="div3">
-                                        <h1 className="divBody">Recruits</h1>
-                                        <h1 className="divBody">4</h1>
+                                    <div>
+                                        <div className="div3">
+                                            <h1 className="divBody">Commission Earned</h1>
+                                            <h1 className="divBody">$76,259</h1>
+                                        </div>
+                                        <div className="div3">
+                                            <h1 className="divBody">Cap Fill</h1>
+                                            <h1 className="divBody">100%</h1>
+                                        </div>
+                                        <div className="div3">
+                                            <h1 className="divBody">Recruits</h1>
+                                            <h1 className="divBody">4</h1>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
