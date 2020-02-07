befaf1e2be116a802c5857f5781810dc36b953f1
Mansoor <hussainmansoor876@gmail.com>
2020-02-07 18:04:43 +0500

    working

--- src/Screens/Header/Header.js
+++ src/Screens/Header/Header.js
@@ -86,9 +86,9 @@ class Header extends React.Component {
                     </div>
                     <div ref={e => this.sideBar = e} class="sidenav">
                         <a href="javascript:void(0)" class="closebtn" onClick={() => this.closeNav()}>&times;</a>
-                        <Link to="#">Dashboard</Link>
-                        <Link to="#">Submission Form</Link>
-                        <Link to="#">Closing Review</Link>
+                        <Link className="sideLink" to="#">Dashboard</Link>
+                        <Link className="sideLink" to="#">Submission Form</Link>
+                        <Link className="sideLink" to="#">Closing Review</Link>
                     </div>
                 </div>
                 <div style={{ display: 'flex', marginTop: 8 }}>
